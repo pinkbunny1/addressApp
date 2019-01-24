1018c73a310b27087cf99c523ff0681ce97257a4
Mason Shin <mshin@coupang.com>
2019-01-24 09:51:54 -0800

    Make edit work with feature first

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,13 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 
-import { addReducer } from './modules/Add';
-import { editReducer } from './modules/Edit';
-import { filterReducer } from './components/Header';
+import { addReducer } from './modules/Add'
+import { editReducer } from './modules/Edit'
+import { filterReducer } from './components/Header'
 
 const rootReducer = combineReducers({
-	add: addReducer,
-	// edit: editReducer, (combined edit and add reducer)
-	filter: filterReducer,
-});
+  add: addReducer,
+  edit: editReducer,
+  filter: filterReducer,
+})
 
-export default rootReducer;
-// export { rootReducer };
+export default rootReducer
